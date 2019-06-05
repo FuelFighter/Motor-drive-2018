728b7a083ab30be31e29a068a7b21f2dfd8fbe12
mahoyen <32592450+mahoyen@users.noreply.github.com>
2019-06-05 19:46:46 +0200

    updated gear_change_speed

--- state_machine.c
+++ state_machine.c
@@ -14,8 +14,8 @@
 #define MIN_VOLT 15.0
 #define MAX_AMP 25.0
 #define MAX_TEMP 100
-#define LOW_GEAR_CHANGE_SPEED 4.0
-#define HIGH_GEAR_CHANGE_SPEED 5.50
+#define LOW_GEAR_CHANGE_SPEED 20//40.0
+#define HIGH_GEAR_CHANGE_SPEED 20//55.0
 
 static uint8_t b_major_fault = 0;
 static uint8_t fault_count = 0;
