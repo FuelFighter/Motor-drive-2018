bba766bbd7396d2237bc0c70f8a2910ca371a740
Haavard <32716576+haavardfiskaa@users.noreply.github.com>
2019-06-07 12:06:34 +0200

    Time-based Torque-Cutoff
    Simple code to shut off drivers after a fixed time in PWM mode.

--- controller.c
+++ controller.c
@@ -4,6 +4,7 @@
  * Created: 18.03.2017 13:30:14
  *  Author: Tanguy Simon for DNV GL Fuel fighter
  */ 
+ */
 
 #include <avr/io.h>
 #include "UniversalModuleDrivers/usbdb.h"
@@ -18,6 +19,8 @@ const float Ki=R*100.0*0.7 ; //100*R
 const float TimeStep = 0.005 ; //5ms (see timer 0 in main.c)
 
 static float f32_Integrator = 0.0 ;
+static uint8_t PWM_Counter = 0; //Counter for shutting of drivers in PWM mode
+//Make a define for counter in motor_controller_selection or similar? 
 
 void reset_I(void)
 {
@@ -30,12 +33,12 @@ void set_I(uint8_t duty)
 }
 
 void controller(volatile ModuleValues_t *vals){
-	
+
 	static float f32_DutyCycleCmd = 50.0 ;
 	float f32_CurrentDelta = 0.0 ;
 	static uint8_t b_saturation = 0;
 	int8_t i8_throttle_cmd = 0;
-	
+
 	if (vals->motor_status == BRAKE)
 	{
 		i8_throttle_cmd = -(int8_t)vals->u8_brake_cmd ;
@@ -44,7 +47,7 @@ void controller(volatile ModuleValues_t *vals){
 	{
 		i8_throttle_cmd = vals->u8_accel_cmd ;
 	}
-	
+
 	if (vals->ctrl_type == CURRENT)
 	{
 		if (f32_DutyCycleCmd >= 95 || f32_DutyCycleCmd <= 50)
@@ -53,20 +56,28 @@ void controller(volatile ModuleValues_t *vals){
 		} else {
 			b_saturation = 0;
 		}
-		
+
 		f32_CurrentDelta = ((float)(i8_throttle_cmd)-vals->f32_motor_current)	;
-		
+
 		if (!b_saturation) // prevents over integration of an error that cannot be dealt with (because the duty cycle reaches a limit) integral windup protection
 		{
 			f32_Integrator+=f32_CurrentDelta*TimeStep ;
 		}
-		
+
 		f32_DutyCycleCmd=Kp*f32_CurrentDelta+f32_Integrator*Ki ;
 		f32_DutyCycleCmd=f32_DutyCycleCmd+50.0 ;
-	
+
 	}else if (vals->ctrl_type == PWM)
 	{
 		f32_DutyCycleCmd = (float)(vals->u8_duty_cycle);
+		if (PWM_Counter < 100)
+		{
+			//100 = 400ms. Must be long enough so that the motor has time to sync,
+			//but short enough so that the actuator does not reach the wheel.
+			//This code is probably better to run in the actuator board.
+			drivers(0); //Shut off driver to avoid applying torque when engaging.
+			PWM_Counter = 0; //Reset PWM_Counter
+		}
 		if (vals->f32_motor_current > 0.5)
 		{
 			//f32_DutyCycleCmd -- ;
@@ -76,19 +87,19 @@ void controller(volatile ModuleValues_t *vals){
 			//f32_DutyCycleCmd ++ ;
 		}
 	}
-	
-	
+
+
 	//bounding of duty cycle for well function of bootstrap capacitors
 	if (f32_DutyCycleCmd > 95)
 	{
 		f32_DutyCycleCmd = 95;
 	}
-	
+
 	if (f32_DutyCycleCmd < 50)// bounding at 50 to prevent rheostatic braking and backwards motion
 	{
 		f32_DutyCycleCmd = 50;
 	}
-	
+
 	if (SW_MODE == BIPOLAR)
 	{
 		OCR3A = (int)((f32_DutyCycleCmd/100.0)*ICR3) ; //PWM_PE3 (non inverted)
@@ -97,7 +108,7 @@ void controller(volatile ModuleValues_t *vals){
 		OCR3A = (int)((f32_DutyCycleCmd/100.0)*ICR3) ; //PWM_PE3
 		OCR3B = (int)(ICR3-(f32_DutyCycleCmd/100.0)*ICR3) ; //PWM_PE4
 	}
-	
+
 	vals->u8_duty_cycle = (uint8_t)f32_DutyCycleCmd ; //exporting the duty cycle to be able to read in on the CAN and USB
 
 }
